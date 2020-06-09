da24f6f7fc756f74bc5a134f57ed84c448bc19d2
Stefano Tamponi <stefanotamponi@outlook.it>
2020-06-09 19:03:23 +0200

    testing functionality (html, js, css) Ok even on build. finishing up with image imports (unnecessary content included)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,5 @@ import "../styles/styles.css";
 // Accept hot modules (CSS Injection)
 if (module.hot) module.hot.accept()
 
+
+document.getElementById("root").insertAdjacentHTML("beforeend", "<h1>Hello World!!</h1>")
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
-const path = require('path'),
+const currentMode = process.env.npm_lifecycle_event;
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 postCSSPlugins = [
   require('postcss-import'),
+  require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
+  require('postcss-hexrgba'),
   require('autoprefixer')
 ]
 
-module.exports = {
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader?url=false', {
+    loader: 'postcss-loader', 
+    options: {
+      plugins: postCSSPlugins
+    }
+  }],
+}
+
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
+  plugins: [new HTMLWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+}
+
+if (currentMode == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
   },
-  devServer: {
+  config.devServer = {
     before: (app, server) => {
       server._watch('./app/**/*.html')
     }, 
@@ -21,18 +48,24 @@ module.exports = {
     port: 3000,
     host: '0.0.0.0'
   },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?url=false', {
-          loader: 'postcss-loader', 
-          options: {
-            plugins: postCSSPlugins
-          }
-        }],
-      }
-    ]
-  }
+  config.mode = 'development'
 }
+
+
+if (currentMode == 'build') {
+  cssConfig.use.unshift(MiniCSSExtractPlugin.loader);
+  postCSSPlugins.push(require('cssnano'));
+
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  config.mode = 'production',
+  config.optimization = {
+    splitChunks: {chunks: 'all'}
+  },
+  config.plugins.push(new CleanWebpackPlugin(), new MiniCSSExtractPlugin({filename: 'styles.[chunkhash].css'}))
+}
+
+module.exports = config;
